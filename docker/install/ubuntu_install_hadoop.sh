d0fe843035095cd936ba6142fe773707f62f4f28
pingsutw <pingsutw@gmail.com>
2020-06-02 11:51:05 +0800

    Install hadoop in deepfm image

---
+++ docker/install/ubuntu_install_hadoop.sh
@@ -0,0 +1,43 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+hadoop_v="2.9.2"
+
+#INSTALL JAVA
+apt-get -q update \
+    && apt-get -q install -y --no-install-recommends openjdk-8-jdk libbcprov-java \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JRE_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
+
+cd /usr/local/ && \
+    wget https://www.apache.org/dist/hadoop/core/hadoop-${hadoop_v}/hadoop-${hadoop_v}.tar.gz && \
+    tar -zxvf hadoop-${hadoop_v}.tar.gz && \
+    mv ./hadoop-${hadoop_v} hadoop
+
+export HADOOP_PREFIX=/usr/local/hadoop
+export HADOOP_COMMON_HOME=/usr/local/hadoop 
+export HADOOP_HDFS_HOME=/usr/local/hadoop
+export HADOOP_HOME=/usr/local/hadoop
+export HADOOP_MAPRED_HOME=/usr/local/hadoop
+export HADOOP_YARN_HOME=/usr/local/hadoop
+export HADOOP_CONF_DIR=/usr/local/hadoop/etc/hadoop
+export YARN_CONF_DIR=/usr/local/hadoop/etc/hadoop
+export PATH=${PATH}:/usr/local/hadoop/bin
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$JAVA_HOME/jre/lib/amd64/server
+export CLASSPATH=$(${HADOOP_HOME}/bin/hadoop classpath --glob)
