0c9a35e16c85cdf6495f5de88075a2d62950e359
Kevin <pingsutw@apache.org>
2021-02-04 14:33:16 +0800

    Add qlib example

--- submarine-sdk/install/ubuntu_install_python_package.sh
+++ submarine-sdk/install/ubuntu_install_python_package.sh
@@ -6,3 +6,4 @@ git clone https://github.com/apache/submarine.git
 pip3 install comet_ml
 pip3 install -U comet_ml[cpu_logging]
 pip3 install submarine/submarine-sdk/pysubmarine
+pip3 install numpy==1.19.5 cython pyqlib==0.6.1
